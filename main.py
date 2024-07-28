e64daef0fff945a1bc8bb870ce4f3c1570901534
KichdiPatel <155916438+KichdiPatel@users.noreply.github.com>
2024-07-28 10:08:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from flask import Flask, render_template
 from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
 from plaid.model.products import Products
-from plaid.model.item_public_token_exchange_request import (
-    ItemPublicTokenExchangeRequest,
-)
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
 from plaid.model.transactions_sync_request import TransactionsSyncRequest
 import pandas as pd
 from datetime import datetime
@@ -14,6 +13,16 @@ from sqlalchemy import Boolean
 from threading import Timer
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
 
 app = Flask(__name__)
 
